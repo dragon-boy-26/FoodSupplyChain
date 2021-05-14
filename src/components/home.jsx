86bb181fe01fe8993c7c82f3e05cd9635a78cd1d
yash-hoke-0602 <yashhoke0206@gmail.com>
2021-05-14 01:47:34 -0700

    Login FrontEnd and Server Created

---
+++ src/components/home.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+
+import axios from 'axios';
+axios.defaults.withCredentials = true;          //https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/withCredentials
+
+class Home extends Component {
+    state = {  
+        loggedUser:"No User"
+    }
+
+    componentDidMount(){
+        console.log("Start");
+        axios.get('http://localhost:3001/isLogin')
+              .then(res =>{
+                  if(res.data.loggedStatus){
+                    this.setState({
+                        loggedUser: res.data.loggedUser
+                    });
+                    console.log(this.state.loggedUser);
+                }
+              });
+    }
+
+    logoutEvent = (e) =>{
+        e.preventDefault();
+        axios.get('http://localhost:3001/logout')
+            .then(res =>{
+                if(res.data === 'logout'){
+                    this.setState({
+                        loggedUser:"No User"
+                    })
+                }
+            });
+
+    }
+
+    logoutButton(){
+        if(this.state.loggedUser !== "No User"){
+            return (<button onClick = {this.logoutEvent}> Logout</button>)
+        }
+        else{
+            return (<div><a href="/login"><button> Login</button></a></div>)
+        }
+    }
+
+    render() { 
+        return (  
+            <div>
+                {this.state.loggedUser}
+                <div>{this.logoutButton()}</div>
+            </div>
+        );
+    }
+}
+ 
+export default Home;
